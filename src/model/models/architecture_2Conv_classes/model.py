c6e3b3588279c8418395aa7221598a9c9e824226
Affapple <joaopmgaspar@ua.pt>
2025-05-28 19:15:33 +0100

    New code

--- src/model/models/architecture_2Conv_classes/model.py
+++ src/model/models/architecture_2Conv_classes/model.py
@@ -1,21 +1,17 @@
 from torch import nn
 import torch.nn.functional as F
 
-class CompleteChessBotNetwork(nn.Module):
-    def __init__(self):
+class old_ChessModel(nn.Module):
+    def __init__(self, num_classes):
         super().__init__()
-        # 13 channels: 12 pieces + side to play (tensor[true's|false's])
-        self.conv1 = nn.Conv2d(13, 64, kernel_size=3, padding='same')
-        self.batchnorm1 = nn.BatchNorm2d(64)
+        # conv1 -> relu -> conv2 -> relu -> flatten -> fc1 -> relu -> fc2
+        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
 
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding='same')
-        self.batchnorm2 = nn.BatchNorm2d(128)
-
-        self.fc1 = nn.Linear(8 * 8 * 128, 256) # 8 * 8 (num of squares) * 128 (num of channels)
-        self.fc2 = nn.Linear(256, 64 * 63) # (Choose 2 squares from the board where the order matters)
-        
-        self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(8 * 8 * 128, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+        self.relu = nn.ReLU()
 
         # Initialize weights
         nn.init.kaiming_uniform_(self.conv1.weight, nonlinearity='relu')
@@ -23,48 +19,17 @@ class CompleteChessBotNetwork(nn.Module):
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
 
-
     def forward(self, x):
         x = self.relu(self.conv1(x))
-        x = self.batchnorm1(x)
-
         x = self.relu(self.conv2(x))
-        x = self.batchnorm2(x)
-
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)  # Output raw logits
         return x
+
 
 class ChessModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # conv1 -> relu -> conv2 -> relu -> flatten -> fc1 -> relu -> fc2
-        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(8 * 8 * 128, 256)
-        self.fc2 = nn.Linear(256, 64*63)
-        self.relu = nn.ReLU()
-
-        # Initialize weights
-        nn.init.kaiming_uniform_(self.conv1.weight, nonlinearity='relu')
-        nn.init.kaiming_uniform_(self.conv2.weight, nonlinearity='relu')
-        nn.init.xavier_uniform_(self.fc1.weight)
-        nn.init.xavier_uniform_(self.fc2.weight)
-
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.flatten(x)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)  # Output raw logits
-        return x
-
-
-class ChessModel_2(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super().__init__()
         # conv1 -> relu -> conv2 -> relu -> flatten -> fc1 -> relu -> fc2
         self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
@@ -75,7 +40,7 @@ class ChessModel_2(nn.Module):
 
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(8 * 8 * 128, 256)
-        self.fc2 = nn.Linear(256, 64*63)
+        self.fc2 = nn.Linear(256, num_classes)
         self.relu = nn.ReLU()
 
         # Initialize weights
